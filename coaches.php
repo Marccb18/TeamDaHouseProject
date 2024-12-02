c02f679b6ca1551b4a68b4ed74a72aeba4e66887
XPlanas <146984835+XPlanas@users.noreply.github.com>
2024-12-02 19:33:05 +0100

    rutas relativas

--- coaches.php
+++ coaches.php
@@ -11,22 +11,22 @@ $tarjetas = json_decode($jsonData, true);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coaches Team Da House</title>
-    <link href="https://unpkg.com/@phosphor-icons/web@2.0.3/dist/phosphor.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="assets/css/slicknav.css">
-    <link rel="stylesheet" href="assets/css/flaticon.css">
-    <link rel="stylesheet" href="assets/css/gijgo.css">
-    <link rel="stylesheet" href="assets/css/animate.min.css">
-    <link rel="stylesheet" href="assets/css/animated-headline.css">
-    <link rel="stylesheet" href="assets/css/magnific-popup.css">
-    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
-    <link rel="stylesheet" href="assets/css/themify-icons.css">
-    <link rel="stylesheet" href="assets/css/slick.css">
-    <link rel="stylesheet" href="assets/css/nice-select.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/coaches.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="./https://unpkg.com/@phosphor-icons/web@2.0.3/dist/phosphor.css" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="./assets/css/slicknav.css">
+    <link rel="stylesheet" href="./assets/css/flaticon.css">
+    <link rel="stylesheet" href="./assets/css/gijgo.css">
+    <link rel="stylesheet" href="./assets/css/animate.min.css">
+    <link rel="stylesheet" href="./assets/css/animated-headline.css">
+    <link rel="stylesheet" href="./assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="./assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="./assets/css/themify-icons.css">
+    <link rel="stylesheet" href="./assets/css/slick.css">
+    <link rel="stylesheet" href="./assets/css/nice-select.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/coaches.css">
+    <link rel="stylesheet" href="./https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body class="black-bg">
@@ -50,24 +50,24 @@ $tarjetas = json_decode($jsonData, true);
                     <div class="menu-wrapper d-flex align-items-center justify-content-between">
                         <!-- Logo -->
                         <div class="logo">
-                            <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
+                            <a href="./index.html"><img src="assets/img/logo/logo.png" alt=""></a>
                         </div>
                         <!-- Main-menu -->
                         <div class="main-menu f-right d-none d-lg-block">
                             <nav>
                                 <ul id="navigation">
-                                    <li><a href="index.html">Home</a></li>
+                                    <li><a href="./index.html">Home</a></li>
 
 
-                                    <li><a href="">About</a></li>
-                                    <li><a href="coaches.php">Coaches</a></li>
-                                    <li><a href="#">Pricing</a></li>
-                                    <li><a href="#">Gallery</a></li>
-                                    <li><a href="#">Blog</a>
+                                    <li><a href="./">About</a></li>
+                                    <li><a href="./coaches.php">Coaches</a></li>
+                                    <li><a href="./#">Pricing</a></li>
+                                    <li><a href="./#">Gallery</a></li>
+                                    <li><a href="./#">Blog</a>
                                         <!-- <ul class="submenu">
-                                            <li><a href="blog.html">Blog</a></li>
-                                            <li><a href="blog_details.html">Blog Details</a></li>
-                                            <li><a href="elements.html">Elements</a></li>
+                                            <li><a href="./blog.html">Blog</a></li>
+                                            <li><a href="./blog_details.html">Blog Details</a></li>
+                                            <li><a href="./elements.html">Elements</a></li>
                                         </ul> -->
                                     </li>
                                 </ul>
@@ -75,7 +75,7 @@ $tarjetas = json_decode($jsonData, true);
                         </div>
                         <!-- Header-btn -->
                         <div class="header-btns d-none d-lg-block f-right">
-                            <a href="contact.html" class="btn">Contacto</a>
+                            <a href="./contact.html" class="btn">Contacto</a>
                         </div>
                         <!-- Mobile Menu -->
                         <div class="col-12">
@@ -100,10 +100,10 @@ $tarjetas = json_decode($jsonData, true);
                             <h3 class="titulo"><?php echo $tarjeta['profesion']; ?></h3>
                             <p><?php echo $tarjeta['descripcion']; ?></p>
                             <div class="iconos">
-                                <a href="<?php echo $tarjeta['facebook']; ?>"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                                <a href="<?php echo $tarjeta['instagram']; ?>"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                                <a href="<?php echo $tarjeta['pinterest']; ?>"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
-                                <a href="<?php echo $tarjeta['twitter']; ?>"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                <a href="./<?php echo $tarjeta['facebook']; ?>"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                <a href="./<?php echo $tarjeta['instagram']; ?>"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                <a href="./<?php echo $tarjeta['pinterest']; ?>"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
+                                <a href="./<?php echo $tarjeta['twitter']; ?>"><i class="fa fa-twitter" aria-hidden="true"></i></a>
                             </div>
                         </div>
                     <?php } endforeach; ?>
@@ -122,7 +122,7 @@ $tarjetas = json_decode($jsonData, true);
                             <div class="single-footer-caption mb-50 text-center">
                                 <!-- logo -->
                                 <div class="footer-logo wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s">
-                                    <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
+                                    <a href="./index.html"><img src="assets/img/logo/logo.png" alt=""></a>
                                 </div>
                                 <!-- Menu -->
                                 <!-- Header Start -->
@@ -133,12 +133,12 @@ $tarjetas = json_decode($jsonData, true);
                                             <div class="main-menu main-menu2 text-center">
                                                 <nav>
                                                     <ul>
-                                                        <li><a href="index.html">Home</a></li>
-                                                        <li><a href="#">About</a></li>
-                                                        <li><a href="#">Courses</a></li>
-                                                        <li><a href="#">Pricing</a></li>
-                                                        <li><a href="#">Gallery</a></li>
-                                                        <li><a href="#">Contact</a></li>
+                                                        <li><a href="./index.html">Home</a></li>
+                                                        <li><a href="./#">About</a></li>
+                                                        <li><a href="./#">Courses</a></li>
+                                                        <li><a href="./#">Pricing</a></li>
+                                                        <li><a href="./#">Gallery</a></li>
+                                                        <li><a href="./#">Contact</a></li>
                                                     </ul>
                                                 </nav>
                                             </div>
@@ -148,8 +148,8 @@ $tarjetas = json_decode($jsonData, true);
                                 <!-- Header End -->
                                 <!-- social -->
                                 <div class="footer-social mt-30 wow fadeInUp" data-wow-duration="3s" data-wow-delay=".8s">
-                                    <a href="#"><i class="fab fa-twitter"></i></a>
-                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="./#"><i class="fab fa-twitter"></i></a>
+                                    <a href="./#"><i class="fab fa-facebook-f"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -175,7 +175,7 @@ $tarjetas = json_decode($jsonData, true);
     </footer>
     <!-- Scroll Up -->
     <div id="back-top">
-        <a title="Go to Top" href="#"> <i class="fas fa-level-up-alt"></i></a>
+        <a title="Go to Top" href="./#"> <i class="fas fa-level-up-alt"></i></a>
     </div>
 
     <!-- JS here -->
